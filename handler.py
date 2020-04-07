f142ec39e73a65911c19539c7645e9b0adfab449
Andrew Hlynskyi <ahlincq@gmail.com>
2020-04-07 19:27:06 +0300

    Removed serving non ALB requests, resolves #31

--- handler.py
+++ handler.py
@@ -70,6 +70,10 @@ def handler(event, context):
     # ALB response is different:
     # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/lambda-functions.html#respond-to-load-balancer
     request_from_lb = bool(event.get("requestContext", {}).get("elb"))
+    if not request_from_lb:
+        logger.error(f"Got event not from ALB: {event}")
+        raise NotImplementedError("An event came not from ALB and cannot be served")
+
     http_method = event.get("httpMethod")
     is_validate_action = event.get("path") == "/validate"
 
@@ -87,7 +91,7 @@ def handler(event, context):
             },
         }
 
-    if request_from_lb and event.get("path") == "/healthz":
+    if event.get("path") == "/healthz":
         return {
             "isBase64Encoded": False,
             "statusCode": 200,
@@ -103,28 +107,18 @@ def handler(event, context):
     except ValueError as error:
         logger.error(error)
 
-        if request_from_lb:
-            return {
-                "isBase64Encoded": False,
-                "statusCode": error.args[1],
-                "statusDescription": str(error.args[1]) + " Server Error",
-                "headers": {
-                    "Content-Type": "text/html",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
-                    "Access-Control-Allow-Methods": "POST,GET,OPTIONS",
-                },
-                "body": error.args[0],
-            }
-        else:
-            return {
-                "body": error.args[0],
-                "headers": {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": True
-                },
-                "statusCode": error.args[1],
-            }
+        return {
+            "isBase64Encoded": False,
+            "statusCode": error.args[1],
+            "statusDescription": str(error.args[1]) + " Server Error",
+            "headers": {
+                "Content-Type": "text/html",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
+                "Access-Control-Allow-Methods": "POST,GET,OPTIONS",
+            },
+            "body": error.args[0],
+        }
 
     # logger.info(pformat(data, indent=2))
 
@@ -156,25 +150,14 @@ def handler(event, context):
 
         link = upload_file_to_s3(filename="archive.zip")
 
-    if request_from_lb:
-        return {
-            "isBase64Encoded": False,
-            "headers": {
-                "Location": link,
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
-                "Access-Control-Allow-Methods": "POST,GET,OPTIONS",
-            },
-            "statusCode": 302,
-            "statusDescription": "302 Found",
-        }
-    else:
-        return {
-            "body": "",
-            "headers": {
-                "Location": link,
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": True
-            },
-            "statusCode": 302,
-        }
+    return {
+        "isBase64Encoded": False,
+        "headers": {
+            "Location": link,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
+            "Access-Control-Allow-Methods": "POST,GET,OPTIONS",
+        },
+        "statusCode": 302,
+        "statusDescription": "302 Found",
+    }
